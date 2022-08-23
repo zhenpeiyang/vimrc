2c50bfe61c83bfdbca9af374aadd3fc20ffc9ae9
Zhenpei Yang <yzp@utexas.edu>
2022-08-23 11:50:10 -0700

    add zsh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+
 ## install vim-plug
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
@@ -8,3 +9,9 @@ cp -r .vim ~/
 cp -r .vimrc ~/
 cp -r .tmux.conf ~/
 cp -r .tmux.dev ~/
+
+sudo apt-get install zsh
+wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh || true
+cp my-hyperzsh.zsh-theme ~/.oh-my-zsh/themes/my-hyperzsh.zsh-theme
+# ENV ZSH_THEME my-hyperzsh
+sed -i "s|robbyrussell|my-hyperzsh|g" ~/.zshrc
